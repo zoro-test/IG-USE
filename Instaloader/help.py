1bdc055f25c44e67a7344fc4c78d40936778c0af
Zoro - Test Account <134167327+zoro-test@users.noreply.github.com>
2023-06-01 18:30:31 +0530

    Update help.py

--- Instaloader/help.py
+++ Instaloader/help.py
@@ -5,9 +5,9 @@ from pyrogram.types import InlineKeyboardMarkup
 
 # Help Message
 @Client.on_message(filters.private & filters.incoming & filters.command("help"))
-async def _help(bot, msg):
+async def _help(bot, message):
     await bot.send_message(
-        msg.chat.id,
+        message.chat.id,
         "**Here's how to use me **\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
